ab962bbba851bb48718be7003085a11fd12caedc
Yana Hasson <yanahasson@gmail.com>
2019-07-11 14:57:58 +0200

    Add pre-commit files

--- libyana/transformutils/colortrans.py
+++ libyana/transformutils/colortrans.py
@@ -1,13 +1,12 @@
 import random
-import torchvision
+import torchvision.transforms.functional as torchfn
 
 
 def get_color_params(brightness=0, contrast=0, saturation=0, hue=0):
     if brightness > 0:
-        brightness_factor = random.uniform(
-            max(0, 1 - brightness), 1 + brightness)
+        bright_factor = random.uniform(max(0, 1 - brightness), 1 + brightness)
     else:
-        brightness_factor = None
+        bright_factor = None
 
     if contrast > 0:
         contrast_factor = random.uniform(max(0, 1 - contrast), 1 + contrast)
@@ -15,16 +14,15 @@ def get_color_params(brightness=0, contrast=0, saturation=0, hue=0):
         contrast_factor = None
 
     if saturation > 0:
-        saturation_factor = random.uniform(
-            max(0, 1 - saturation), 1 + saturation)
+        sat_factor = random.uniform(max(0, 1 - saturation), 1 + saturation)
     else:
-        saturation_factor = None
+        sat_factor = None
 
     if hue > 0:
         hue_factor = random.uniform(-hue, hue)
     else:
         hue_factor = None
-    return brightness_factor, contrast_factor, saturation_factor, hue_factor
+    return bright_factor, contrast_factor, sat_factor, hue_factor
 
 
 def color_jitter(img, brightness=0, contrast=0, saturation=0, hue=0):
@@ -32,19 +30,25 @@ def color_jitter(img, brightness=0, contrast=0, saturation=0, hue=0):
         brightness=brightness,
         contrast=contrast,
         saturation=saturation,
-        hue=hue)
+        hue=hue,
+    )
 
     # Create img transform function sequence
     img_transforms = []
     if brightness is not None:
-        img_transforms.append(lambda img: torchvision.transforms.functional.adjust_brightness(img, brightness))
-    if saturation is not None:
-        img_transforms.append(lambda img: torchvision.transforms.functional.adjust_saturation(img, saturation))
-    if hue is not None:
         img_transforms.append(
-            lambda img: torchvision.transforms.functional.adjust_hue(img, hue))
+            lambda img: torchfn.adjust_brightness(img, brightness)
+        )
+    if saturation is not None:
+        img_transforms.append(
+            lambda img: torchfn.adjust_saturation(img, saturation)
+        )
+    if hue is not None:
+        img_transforms.append(lambda img: torchfn.adjust_hue(img, hue))
     if contrast is not None:
-        img_transforms.append(lambda img: torchvision.transforms.functional.adjust_contrast(img, contrast))
+        img_transforms.append(
+            lambda img: torchfn.adjust_contrast(img, contrast)
+        )
     random.shuffle(img_transforms)
 
     jittered_img = img
