215c083b03a296b0e76054fec64b6eda493341c2
Yana Hasson <yanahasson@gmail.com>
2019-07-11 14:29:06 +0200

    Add generic hand utils from handobjectdatasets

---
+++ libyana/visutils/vizmp3d.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+def cam_equal_aspect_3d(ax, verts, flip_x=False):
+    """
+    Centers view on cuboid containing verts and flips y and z axis
+    and fixes azimuth
+    """
+    extents = np.stack([verts.min(0), verts.max(0)], axis=1)
+    sz = extents[:, 1] - extents[:, 0]
+    centers = np.mean(extents, axis=1)
+    maxsize = max(abs(sz))
+    r = maxsize / 2
+    if flip_x:
+        ax.set_xlim(centers[0] + r, centers[0] - r)
+    else:
+        ax.set_xlim(centers[0] - r, centers[0] + r)
+    # Invert y and z axis
+    ax.set_ylim(centers[1] + r, centers[1] - r)
+    ax.set_zlim(centers[2] + r, centers[2] - r)
