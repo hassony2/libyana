9e767b1b5530b18bf52e41f0374ddb6f57eff35f
Yana <yanahasson@gmail.com>
2019-07-16 09:54:28 +0200

    Add add_mesh

--- libyana/visutils/vizmp3d.py
+++ libyana/visutils/vizmp3d.py
@@ -1,3 +1,5 @@
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import numpy as np
 
 
@@ -18,3 +20,31 @@ def cam_equal_aspect_3d(ax, verts, flip_x=False):
     # Invert y and z axis
     ax.set_ylim(centers[1] + r, centers[1] - r)
     ax.set_zlim(centers[2] + r, centers[2] - r)
+
+
+def add_mesh(ax, verts, faces, flip_x=False, c="b", alpha=0.1):
+    ax.view_init(elev=90, azim=-90)
+    mesh = Poly3DCollection(verts[faces], alpha=alpha)
+    if c == "b":
+        face_color = (141 / 255, 184 / 255, 226 / 255)
+        edge_color = (0 / 255, 0 / 255, 112 / 255)
+    elif c == "r":
+        face_color = (226 / 255, 141 / 255, 141 / 255)
+        edge_color = (112 / 255, 0 / 255, 0 / 255)
+    elif c == "viridis":
+        face_color = plt.cm.viridis(np.linspace(0, 1, faces.shape[0]))
+        edge_color = None
+        edge_color = (0 / 255, 0 / 255, 112 / 255)
+    elif c == "plasma":
+        face_color = plt.cm.plasma(np.linspace(0, 1, faces.shape[0]))
+        edge_color = None
+        # edge_color = (0 / 255, 0 / 255, 112 / 255)
+    else:
+        face_color = c
+        edge_color = c
+
+    mesh.set_edgecolor(edge_color)
+    mesh.set_facecolor(face_color)
+    ax.add_collection3d(mesh)
+    cam_equal_aspect_3d(ax, verts, flip_x=flip_x)
+    plt.tight_layout()
