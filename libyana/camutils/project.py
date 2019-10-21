548cdf19875e30e7975c84430bb89d8362d580b0
Yana <yanahasson@gmail.com>
2019-10-21 10:51:15 +0200

    Add batch_proj2d for camutils.project

---
+++ libyana/camutils/project.py
@@ -0,0 +1,7 @@
+def batch_proj2d(verts, camintr, camextr=None):
+    # Project 3d vertices on image plane
+    if camextr is not None:
+        verts = camextr.bmm(verts.transpose(1, 2)).transpose(1, 2)
+    verts_hom2d = camintr.bmm(verts.transpose(1, 2)).transpose(1, 2)
+    proj_verts2d = verts_hom2d[:, :, :2] / verts_hom2d[:, :, 2:]
+    return proj_verts2d
