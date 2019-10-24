e89d61ed6b18764b51927b14e10c3c837df5acb5
Yana <yanahasson@gmail.com>
2019-10-24 18:42:41 +0200

    Add option scale=False to meshnorm

--- libyana/flowutils/dispflow.py
+++ libyana/flowutils/dispflow.py
@@ -2,6 +2,7 @@ import math
 
 from matplotlib.colors import hsv_to_rgb
 import numpy as np
+import torch
 
 
 def radial_flow(flow):
@@ -26,3 +27,23 @@ def disp_flow(flow):
     hsv = np.stack((flow_ang, flow_mag, np.ones_like(flow_ang)), 2)
     rgb = hsv_to_rgb(hsv)  # In range [0, 1]
     return rgb
+
+
+def batch_radial_flow(flow):
+    flow_mag = flow.norm(2, -1)
+    flow_x = flow[:, :, :, 0]
+    flow_y = flow[:, :, :, 1]
+    flow_ang = torch.atan2(flow_x, flow_y)
+    return flow_ang, flow_mag
+
+
+def batch_normalize_flow(flow_ang, flow_mag):
+    min_ang = -math.pi
+    max_ang = math.pi
+    flow_ang = (flow_ang - min_ang) / (max_ang - min_ang)
+    flow_min = flow_mag.view(flow_mag.shape[0], -1).min(1)[0]
+    flow_max = flow_mag.view(flow_mag.shape[0], -1).max(1)[0]
+    flow_mag = (flow_mag - flow_min.view(flow_mag.shape[0], 1, 1)) / (
+        flow_max - flow_min
+    ).view(flow_mag.shape[0], 1, 1)
+    return flow_ang, flow_mag
--- libyana/meshutils/meshnorm.py
+++ libyana/meshutils/meshnorm.py
@@ -1,19 +1,25 @@
 import numpy as np
 
 
-def center_vert_bbox(vertices, bbox_center=None, bbox_scale=None):
+def center_vert_bbox(vertices, bbox_center=None, bbox_scale=None, scale=True):
     if bbox_center is None:
         bbox_center = (vertices.min(0) + vertices.max(0)) / 2
     vertices = vertices - bbox_center
-    if bbox_scale is None:
-        bbox_scale = np.linalg.norm(vertices, 2, 1).max()
-    vertices = vertices / bbox_scale
+    if scale:
+        if bbox_scale is None:
+            bbox_scale = np.linalg.norm(vertices, 2, 1).max()
+        vertices = vertices / bbox_scale
+    else:
+        bbox_scale = 1
     return vertices, bbox_center, bbox_scale
 
 
-def center_vert_pt(vertices):
+def center_vert_pt(vertices, scale=True):
     bbox_center = (vertices.min(0)[0] + vertices.max(0)[0]) / 2
     vertices = vertices - bbox_center
-    max_norm = vertices.norm(2, 1).max()
-    vertices = vertices / max_norm
+    if scale:
+        max_norm = vertices.norm(2, 1).max()
+        vertices = vertices / max_norm
+    else:
+        max_norm = 1
     return vertices, bbox_center, max_norm
