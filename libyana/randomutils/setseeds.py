ed756754fbe9a9179666f47435f5faef71b8499c
Yana <yanahasson@gmail.com>
2019-11-03 12:11:58 +0100

    Add setseeds.set_all_seeeds

---
+++ libyana/randomutils/setseeds.py
@@ -0,0 +1,11 @@
+import random
+
+import torch
+import numpy as np
+
+
+def set_all_seeds(seed):
+    torch.cuda.manual_seed_all(seed)
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
