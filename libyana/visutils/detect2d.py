50f8efe34e9ba5648a69b176c748e28a7fc816bf
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-02-25 14:59:02 +0000

    Add box label_color

--- libyana/visutils/detect2d.py
+++ libyana/visutils/detect2d.py
@@ -1,7 +1,13 @@
 from matplotlib import pyplot as plt
 
 
-def visualize_bbox(ax, box, color="r", label=None, alpha=1, linewidth=4):
+def visualize_bbox(ax,
+                   box,
+                   color="r",
+                   label=None,
+                   alpha=1,
+                   linewidth=4,
+                   label_color=None):
     """
     Args:
         box: [x_min, y_min, x_max, y_max]
@@ -15,19 +21,31 @@ def visualize_bbox(ax, box, color="r", label=None, alpha=1, linewidth=4):
             linewidth=linewidth,
             color=color,
             alpha=alpha,
-        )
-    )
-    ax.annotate(
-        f"{label}", (box[0], box[1] + 4), color=color, weight="bold", alpha=alpha
-    )
+        ))
+    if label_color is None:
+        label_color = color
+    ax.annotate(f"{label}", (box[0], box[1] + 4),
+                color=label_color,
+                weight="bold",
+                alpha=alpha)
 
 
-def visualize_bboxes(ax, bboxes, color="r", labels=None, alpha=1, linewidth=4):
+def visualize_bboxes(ax,
+                     bboxes,
+                     color="r",
+                     labels=None,
+                     alpha=1,
+                     linewidth=4,
+                     label_color=None):
     for box_idx, box in enumerate(bboxes):
         if labels is None:
             label = None
         else:
             label = labels[box_idx]
-        visualize_bbox(
-            ax, box, label=label, color="r", alpha=alpha, linewidth=linewidth
-        )
+        visualize_bbox(ax,
+                       box,
+                       label=label,
+                       color="r",
+                       alpha=alpha,
+                       linewidth=linewidth,
+                       label_color=label_color)
