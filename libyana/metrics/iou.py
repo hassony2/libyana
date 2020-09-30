3a29d9290fe757b126db33fb4041597044bc3676
Yana <yanahasson@gmail.com>
2020-09-30 10:58:04 +0200

    Add batch_mask_iou

--- libyana/metrics/iou.py
+++ libyana/metrics/iou.py
@@ -1,3 +1,23 @@
+def batch_mask_iou(ref, pred, eps=0.000001):
+    ref = ref.float()
+    pred = pred.float()
+    if ref.max() > 1 or ref.min() < 0:
+        raise ValueError(
+            "Ref mask should have values in [0, 1], "
+            f"not [{ref.min(), ref.max()}]"
+        )
+    if pred.max() > 1 or pred.min() < 0:
+        raise ValueError(
+            "Ref mask should have values in [0, 1], "
+            f"not [{pred.min(), pred.max()}]"
+        )
+
+    inter = ref * pred
+    union = (ref + pred).clamp(0, 1)
+    ious = inter.sum(1).sum(1).float() / (union.sum(1).sum(1).float() + eps)
+    return ious
+
+
 def get_area(bbox):
     area = (bbox[2] - bbox[0]) * (bbox[3] - bbox[1])
     return area
