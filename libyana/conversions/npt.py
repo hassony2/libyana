b2bb1dc7253316985be1ef04aa68710bbb039a2f
Yana <yanahasson@gmail.com>
2020-10-26 22:22:24 +0100

    Add point rotations

--- libyana/conversions/npt.py
+++ libyana/conversions/npt.py
@@ -5,3 +5,11 @@ def numpify(tensor):
     if isinstance(tensor, torch.Tensor):
         tensor = tensor.detach().cpu().numpy()
     return tensor
+
+
+def tensorify(array, device=None):
+    if not isinstance(array, torch.Tensor):
+        array = torch.tensor(array)
+    if device is not None:
+        array = array.to(device)
+    return array
---
+++ libyana/lib3d/trans3d.py
@@ -0,0 +1,37 @@
+from pytorch3d import transforms as py3dt
+
+from libyana.conversion import npt
+
+
+def rot_points(points, centers=None, axisang=(0, 1, 1)):
+    """
+    Rotate points around centers
+
+    Args:
+        points (torch.Tensor): (batch_size, point_nb, 3) points to rotate
+    """
+    points = npt.tensorify(points)
+    if points.dim() != 3 or (points.shape[2] != 3):
+        raise ValueError(
+            "Expected batch of vertices in format (batch_size, vert_nb, 3)"
+            f" but got {points.shape}"
+        )
+
+    if centers is None:
+        centers = points.mean(1)
+    else:
+        centers = npt.tensorify(centers, points.device)
+
+    if centers.dim() == 2:
+        centers = centers.unsqueeze(1)
+    points_c = points - centers
+    rot_mats = py3dt.so3_exponential_map(
+        points.new(axisang).unsqueeze(0)
+    ).view(1, 3, 3)
+    points_cr = (
+        rot_mats.repeat(points.shape[0], 1, 1)
+        .bmm(points_c.transpose(1, 2))
+        .transpose(1, 2)
+    )
+    points_final = points_cr + centers
+    return points_final
