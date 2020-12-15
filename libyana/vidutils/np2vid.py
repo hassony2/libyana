6105b76da8afc402016f180c83ae7134dd699a5d
Yana Hasson <yanahasson@gmail.com>
2020-12-15 16:54:33 +0100

    Fix indent

--- libyana/vidutils/np2vid.py
+++ libyana/vidutils/np2vid.py
@@ -18,10 +18,10 @@ def make_video(imgs, video_paths, fps=2, resize_factor=1, verbose=True):
     clip = editor.ImageSequenceClip(imgs, fps=fps)
     if isinstance(video_paths, str):
         video_paths = [video_paths]
-        for video_path in video_paths:
-            if video_path.endswith(".gif"):
-                clip.write_gif(str(video_path))
-            else:
-                clip.write_videofile(str(video_path))
-            if verbose:
-                print(f"Saved video to {video_path}")
+    for video_path in video_paths:
+        if video_path.endswith(".gif"):
+            clip.write_gif(str(video_path))
+        else:
+            clip.write_videofile(str(video_path))
+        if verbose:
+            print(f"Saved video to {video_path}")
